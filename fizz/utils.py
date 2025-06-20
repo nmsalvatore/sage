2ee13a643f6c72af82b74fb698cf8263b276ac6b
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-20 16:55:01 -0700

    refactor: better time_in_seconds with more robust test

--- fizz/utils.py
+++ fizz/utils.py
@@ -1,2 +1,5 @@
 def time_in_seconds(hours=0, minutes=0, seconds=0):
-    return hours * 60 * 60
+    """
+    Converts hours, minutes, and seconds to total seconds.
+    """
+    return int(hours * 3600) + int(minutes * 60) + int(seconds)
--- tests/unit/test_timer.py
+++ tests/unit/test_timer.py
@@ -4,5 +4,16 @@ from fizz.utils import time_in_seconds
 
 
 class TestTimer(TestCase):
+    """
+    Testing suite for fizz timer.
+    """
+
     def test_time_in_seconds(self):
-        self.assertEqual(time_in_seconds(1), 3600)
+        """
+        Test utility function `time_in_seconds` which converts hours,
+        minutes and seconds to total seconds.
+        """
+        self.assertEqual(time_in_seconds(hours=1), 3600)
+        self.assertEqual(time_in_seconds(hours=1, minutes=30), 5400)
+        self.assertEqual(time_in_seconds(hours=2, seconds=5), 7205)
+        self.assertEqual(time_in_seconds(minutes=30, seconds=25), 1825)
