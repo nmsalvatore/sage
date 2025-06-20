dc75911dc7a5169691262374925d1452f69ace58
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-20 13:43:53 -0700

    feat: changed cli function to fizz, reflects cli command

--- fizz/main.py
+++ fizz/main.py
@@ -3,20 +3,20 @@ import click
 
 @click.group()
 @click.version_option()
-def cli():
+def fizz():
     pass
 
-@cli.command()
+@fizz.command()
 @click.option("-h", "--hours", type=int)
 @click.option("-m", "--minutes", type=int)
 @click.option("-s", "--seconds", type=int)
 def timer(hours=0, minutes=0, seconds=0):
     click.echo(f"{seconds} seconds")
 
-@cli.command()
+@fizz.command()
 def stopwatch():
     pass
 
 
 if __name__ == "__main__":
-    cli()
+    fizz()
