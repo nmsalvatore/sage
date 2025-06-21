fdd6cfe9dc27b4a7f336b71e7d29aac2ae7316be
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-20 22:48:05 -0700

    feat: added function to expand a time in seconds to hours, minutes, seconds

--- fizz/utils.py
+++ fizz/utils.py
@@ -1,5 +1,15 @@
-def time_in_seconds(hours=0, minutes=0, seconds=0):
+def convert_time_to_seconds(hours=0, minutes=0, seconds=0) -> int:
     """
     Converts hours, minutes, and seconds to total seconds.
     """
-    return int(hours * 3600) + int(minutes * 60) + int(seconds)
+    return (hours * 3600) + (minutes * 60) + (seconds)
+
+
+def expand_time_from_seconds(total_seconds) -> tuple:
+    """
+    Expand a time in seconds to hours, minutes, and seconds.
+    """
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds - (hours * 3600)) // 60)
+    seconds = int(total_seconds % 60)
+    return (hours, minutes, seconds)
--- tests/unit/test_timer.py
+++ tests/unit/test_timer.py
@@ -1,19 +1,28 @@
 from unittest import TestCase
 
-from fizz.utils import time_in_seconds
+from fizz.utils import convert_time_to_seconds, expand_time_from_seconds
 
 
-class TestTimer(TestCase):
+class TestBasicTimer(TestCase):
     """
     Testing suite for fizz timer.
     """
 
-    def test_time_in_seconds(self):
+    def test_convert_time_to_seconds(self):
         """
-        Test utility function `time_in_seconds` which converts hours,
+        Test utility function `convert_time_to_seconds` which converts hours,
         minutes and seconds to total seconds.
         """
-        self.assertEqual(time_in_seconds(hours=1), 3600)
-        self.assertEqual(time_in_seconds(hours=1, minutes=30), 5400)
-        self.assertEqual(time_in_seconds(hours=2, seconds=5), 7205)
-        self.assertEqual(time_in_seconds(minutes=30, seconds=25), 1825)
+        self.assertEqual(convert_time_to_seconds(hours=1), 3600)
+        self.assertEqual(convert_time_to_seconds(hours=1, minutes=30), 5400)
+        self.assertEqual(convert_time_to_seconds(hours=2, seconds=5), 7205)
+        self.assertEqual(convert_time_to_seconds(minutes=30, seconds=25), 1825)
+
+    def test_expand_time_from_seconds(self):
+        """
+        Test utility function `expand_time_from_seconds` which expands
+        a time in seconds to hours, minutes, and seconds.
+        """
+        self.assertEqual(expand_time_from_seconds(65), (0, 1, 5))
+        self.assertEqual(expand_time_from_seconds(3657), (1, 0, 57))
+        self.assertEqual(expand_time_from_seconds(28933), (8, 2, 13))
