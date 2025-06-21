8f4a43b1f74deeb417030b2ebf22458851eadc11
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-20 22:51:20 -0700

    refactor: remove whitespace beneath function docstrings

--- tests/functional/test_fizz_cli.py
+++ tests/functional/test_fizz_cli.py
@@ -6,7 +6,6 @@ def test_fizz_version():
     To check that fizz is installed correctly, a user uses the `fizz
     --version` command.
     """
-
     result = subprocess.run(
         ["fizz", "--version"], capture_output=True, text=True, timeout=5
     )
@@ -19,7 +18,6 @@ def test_fizz_help():
     To get familiar with basic fizz usage, a user uses the `fizz
     --help` command.
     """
-
     result = subprocess.run(
         ["fizz", "--help"], capture_output=True, text=True, timeout=5
     )
@@ -35,7 +33,6 @@ def test_fizz_timer_help():
     Now familiar with the basic fizz commands, the user checks to see how
     to use the timer command with `fizz timer --help`.
     """
-
     result = subprocess.run(
         ["fizz", "timer", "--help"], capture_output=True, text=True, timeout=5
     )
