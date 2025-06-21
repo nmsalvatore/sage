9aae54ce498b1c7fd7ebf55d5db53054853ffea4
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-21 08:11:25 -0700

    style: set time color to GREEN

--- fizz/main.py
+++ fizz/main.py
@@ -3,29 +3,31 @@ import time
 
 import click
 
-from .utils import convert_time_to_seconds, expand_time_from_seconds
+from . import utils
 
 
-def run_timer(stdscr, hours=0, minutes=0, seconds=0):
+def load_timer(stdscr, hours=0, minutes=0, seconds=0):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     stdscr.clear()
     curses.curs_set(0)  # Hide terminal cursor
 
     start = time.perf_counter()
-    timer_in_seconds = convert_time_to_seconds(hours, minutes, seconds)
+    timer_in_seconds = utils.convert_time_to_seconds(hours, minutes, seconds)
 
     while True:
         elapsed = time.perf_counter() - start
         time_remaining = timer_in_seconds - elapsed
 
-        hours, minutes, seconds = expand_time_from_seconds(time_remaining)
-        formatted_time_remaining = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+        hours, minutes, seconds = utils.expand_time_from_seconds(time_remaining)
+        ftime_remaining = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
-        # Calculate the center y and x positions where the remaining
-        # time will be rendered
         stdscr.addstr(
             curses.LINES // 2,
-            curses.COLS // 2 - len(formatted_time_remaining) // 2,
-            formatted_time_remaining,
+            curses.COLS // 2 - len(ftime_remaining) // 2,
+            ftime_remaining,
+            curses.color_pair(1),
         )
 
         if time_remaining < 0:
@@ -38,10 +40,6 @@ def run_timer(stdscr, hours=0, minutes=0, seconds=0):
 @click.group()
 @click.version_option()
 def fizz():
-    """
-    The base fizz command, executed with `fizz`. Includes `--help` and
-    `--version` flags which supply their respective information.
-    """
     pass
 
 
@@ -50,19 +48,11 @@ def fizz():
 @click.option("-m", "--minutes", type=int, default=0)
 @click.option("-s", "--seconds", type=int, default=0)
 def timer(**kwargs):
-    """
-    The fizz timer, executed with `fizz timer` and displayed with the
-    curses library.
-    """
-    curses.wrapper(lambda stdscr: run_timer(stdscr, **kwargs))
+    curses.wrapper(lambda stdscr: load_timer(stdscr, **kwargs))
 
 
 @fizz.command()
 def stopwatch():
-    """
-    The fizz stopwatch, executed with `fizz stopwatch` and displayed
-    with the curses library.
-    """
     pass
 
 
