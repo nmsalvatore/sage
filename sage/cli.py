ca61b17542ee4fe515b8a938a8f1fde07cdfe571
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-22 18:50:36 -0700

    docs: removed docstring for timer command

--- sage/cli.py
+++ sage/cli.py
@@ -19,12 +19,6 @@ def sage():
 @click.argument("time_string", required=False)
 @click.option("--test", is_flag=True, hidden=True)
 def timer(test, **kwargs):
-    """
-    The sage `timer` command can load a timer using option flags, a
-    single human-readable string, or a custom timer name. For testing
-    purposes, `timer` also includes a `--test` flag which will echo
-    the total calculated time of the timer being called.
-    """
     if test:
         time_in_seconds = get_timer_duration(**kwargs)
         click.echo(format_time(time_in_seconds))
