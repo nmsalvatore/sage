7e4ee809ff168c8325b07cacc784529b6dd75fcd
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-22 21:41:36 -0700

    feat: added done message and timer name to curses pane

--- sage/timer.py
+++ sage/timer.py
@@ -31,9 +31,16 @@ def get_timer_duration(hours=0, minutes=0, seconds=0, time_string=None) -> int:
 def load_timer(stdscr, hours=0, minutes=0, seconds=0, time_string=None):
     curses.start_color()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
     stdscr.clear()
     curses.curs_set(0)  # Hide terminal cursor
+
+    timer_name = ""
+    saved_timer = get_saved_timer(time_string)
+    if saved_timer:
+        timer_name = time_string
 
     start = time.perf_counter()
     total_seconds = get_timer_duration(hours, minutes, seconds, time_string)
@@ -41,9 +48,14 @@ def load_timer(stdscr, hours=0, minutes=0, seconds=0, time_string=None):
     while True:
         elapsed = time.perf_counter() - start
         time_remaining = total_seconds - elapsed
-        ftime_remaining = format_time_as_clock(time_remaining)
 
+        ftime_remaining = format_time_as_clock(time_remaining)
         ftime_y, ftime_x = get_curses_center_positions(ftime_remaining)
+
+        if timer_name:
+            _, timer_name_x = get_curses_center_positions(timer_name)
+            stdscr.addstr(ftime_y - 1, timer_name_x, timer_name, curses.color_pair(3))
+
         stdscr.addstr(ftime_y, ftime_x, ftime_remaining, curses.color_pair(1))
 
         if time_remaining < 0:
@@ -51,3 +63,8 @@ def load_timer(stdscr, hours=0, minutes=0, seconds=0, time_string=None):
 
         time.sleep(0.1)
         stdscr.refresh()
+
+    message = "Time's up! Press any key to quit."
+    message_y, message_x = get_curses_center_positions(message)
+    stdscr.addstr(message_y, message_x, message, curses.color_pair(2))
+    stdscr.getch()
