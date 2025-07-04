93bcf1eb79363e3fa53d6e40813272567d64be44
Nick Salvatore <nmsalvatore@gmail.com>
2025-07-04 07:47:05 -0700

    feat: added pause and quit in timer interface

--- sage/timer.py
+++ sage/timer.py
@@ -37,16 +37,22 @@ def load_timer(stdscr, hours=0, minutes=0, seconds=0, time_string=None):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     stdscr.clear()
     curses.curs_set(0)  # Hide terminal cursor
+    stdscr.nodelay(1)   # Make getch() non-blocking
 
     timer_name = ""
     saved_timer = get_saved_timer(time_string)
     if saved_timer:
         timer_name = time_string
 
-    start = time.perf_counter()
+    start_time = time.perf_counter()
+    pause_time = 0
+    paused = False
+    pause_start = 0
+
     total_seconds = get_timer_duration(hours, minutes, seconds, time_string)
 
     # add one second to total seconds so that the timer duration
@@ -57,17 +63,45 @@ def load_timer(stdscr, hours=0, minutes=0, seconds=0, time_string=None):
     y = 0
 
     while True:
-        elapsed = time.perf_counter() - start
-        time_remaining = total_seconds - elapsed
+        key = stdscr.getch()
 
+        if key == ord(" "):
+            if not paused:
+                pause_start += time.perf_counter()
+                paused = True
+            else:
+                pause_time += time.perf_counter() - pause_start
+                paused = False
+        elif key == ord("q"):
+            break
+
+        paused_message = "Paused"
+        paused_y, paused_x = get_curses_center_positions(paused_message)
+        paused_y += 1
+
+        if paused:
+            elapsed = pause_start - start_time - pause_time
+            stdscr.addstr(paused_y, paused_x, paused_message, curses.color_pair(4))
+        else:
+            elapsed = time.perf_counter() - start_time - pause_time
+            stdscr.move(paused_y, paused_x)
+            stdscr.clrtoeol()
+            pause_start = 0
+
+        time_remaining = total_seconds - elapsed
         ftime_remaining = format_time_as_clock(time_remaining)
         y, ftime_x = get_curses_center_positions(ftime_remaining)
 
         if timer_name:
             _, timer_name_x = get_curses_center_positions(timer_name)
-            stdscr.addstr(1, 1, timer_name, curses.color_pair(2))
+
+            if len(timer_name) % 2:
+                timer_name_x -= 1
+
+            stdscr.addstr(y - 1, timer_name_x, timer_name, curses.color_pair(2))
 
         stdscr.addstr(y, ftime_x, ftime_remaining, curses.color_pair(1))
+        stdscr.addstr(curses.LINES - 1, 1, "<q> Quit, <Space> Pause/Resume, <r> Restart", curses.color_pair(3))
 
         if time_remaining < 1:
             break
