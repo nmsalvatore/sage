91fe819bf63a52dac4f3c9a36c3d492d404ddb8a
Nick Salvatore <nmsalvatore@gmail.com>
2025-07-12 20:28:14 -0700

    refactor: updated get_duration function name from get_timer_duration

--- tests/unit/test_timer.py
+++ tests/unit/test_timer.py
@@ -10,11 +10,11 @@ class TestTimer(TestCase):
 
     def test_get_timer_duration(self):
         """
-        Test that `get_timer_duration` gets the correct time, depending
+        Test that `get_duration` gets the correct time, depending
         on how the arguments are passed to `sage timer`.
         """
         timer = Timer()
-        self.assertEqual(timer.get_timer_duration(time_string="2m 3s"), 123)
-        self.assertEqual(timer.get_timer_duration(hours=1), 3600)
-        self.assertEqual(timer.get_timer_duration(0, 5), 300)
-        self.assertEqual(timer.get_timer_duration(time_string="2 minute 60s"), 180)
+        self.assertEqual(timer.get_duration(time_string="2m 3s"), 123)
+        self.assertEqual(timer.get_duration(hours=1), 3600)
+        self.assertEqual(timer.get_duration(0, 5), 300)
+        self.assertEqual(timer.get_duration(time_string="2 minute 60s"), 180)
