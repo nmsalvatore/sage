5ca047b53fa3bc305a2b49ecf27d781cc6d1237a
Nick Salvatore <nmsalvatore@gmail.com>
2025-07-19 09:45:02 -0700

    refactor: cleaned up conversion functionality with tests

--- sage/common/convert.py
+++ sage/common/convert.py
@@ -1,5 +1,6 @@
 import re
 from textwrap import dedent
+from click.exceptions import BadArgumentUsage
 
 
 def time_units_to_seconds(hours=0, minutes=0, seconds=0) -> int:
@@ -18,14 +19,14 @@ def time_string_to_seconds(time_string: str) -> int:
     seconds = re.search(r"(\d+)\s*(s|sec|second|seconds)", time_string)
 
     if not any([hours, minutes, seconds]):
-        raise ValueError(
+        raise BadArgumentUsage(
             dedent(
                 f"'{time_string}' is not a valid time format. "
                 "Please use formats like '25m', '1h 30m', or '45s'."
-            )
-        )
+            ))
 
     total = 0
+
     if hours:
         total += int(hours.group(1)) * 3600
     if minutes:
@@ -34,12 +35,11 @@ def time_string_to_seconds(time_string: str) -> int:
         total += int(seconds.group(1))
 
     if total == 0:
-        raise ValueError(
+        raise BadArgumentUsage(
             dedent(
                 "Please check your time values (e.g., use '1s' instead of '0s'). "
                 "Time must be greater than 0 seconds."
-            )
-        )
+            ))
 
     return total
 
